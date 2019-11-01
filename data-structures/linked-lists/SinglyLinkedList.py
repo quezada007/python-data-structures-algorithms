d4dcab0dff00cb843effb9d93d74bb23e8792e68
Jose Quezada <quezada007@yahoo.com>
2019-10-31 21:28:38 -0700

    initial commit

---
+++ data-structures/linked-lists/SinglyLinkedList.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def insert_first(self, data):
+        pass
+
+    def insert_last(self, data):
+        pass
+
+    def insert(self, data, position):
+        pass
+
+    def get_first(self):
+        pass
+
+    def get_last(self):
+        pass
+
+    def get_position(self, position):
+        pass
+
+    def remove_first(self):
+        pass
+
+    def remove_last(self):
+        pass
+
+    def remove(self, data):
+        pass
+
+    def remove_position(self, position):
+        pass
+
+    def size(self):
+        pass
+
+    def traverse_list(self):
+        pass
---
+++ data-structures/linked-lists/test_SinglyLinkedList.py
@@ -0,0 +1,21 @@
+import unittest
+from SinglyLinkedList import SinglyLinkedList
+
+
+class TestSinglyLinkedList(unittest.TestCase):
+
+    def setUp(self):
+        self.ll = SinglyLinkedList()
+
+    def test_insert_first(self):
+        pass
+
+    def test_insert_last(self):
+        pass
+
+    def test_insert(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
