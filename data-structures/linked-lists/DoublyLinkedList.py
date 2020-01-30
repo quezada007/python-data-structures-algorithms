65bb2dcaf14cd6ebe7c31ffb34593f2a5c47c4ff
Jose Quezada <quezada007@yahoo.com>
2020-01-29 20:02:35 -0800

    List all the methods for the Doubly Linked List that will get implemented

--- data-structures/linked-lists/DoublyLinkedList.py
+++ data-structures/linked-lists/DoublyLinkedList.py
@@ -25,3 +25,36 @@ class DoublyLinkedList:
         """
         self.head = None
         self.size = 0
+
+    def insert_first(self, data):
+        pass
+
+    def insert_last(self, data):
+        pass
+
+    def insert_after(self, data, position):
+        pass
+
+    def insert_before(self, data, position):
+        pass
+
+    def remove_first(self):
+        pass
+
+    def remove_last(self):
+        pass
+
+    def remove(self, data):
+        pass
+
+    def remove_position(self, position):
+        pass
+
+    def get_size(self):  # O(1)
+        pass
+
+    def traverse_list(self):  # O(n)
+        pass
+
+    def reverse_list(self):  # O(n)
+        pass
