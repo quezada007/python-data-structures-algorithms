040d1721db913e6ed01830070d4751543da2758d
Jose Quezada <quezada007@yahoo.com>
2020-01-31 06:41:09 -0800

    Add Circular Linked List

---
+++ data-structures/linked-lists/CircularLinkedList.py
@@ -0,0 +1,41 @@
+class Node:
+    """
+    The Node for the Circular Linked List
+    """
+
+    def __init__(self, data):
+        """
+        The Node constructor
+
+        :param data: The node data
+        """
+        self.data = data
+        self.next = None
+
+
+class CircularLinkedList:
+
+    def __init__(self):
+        self.tail = None
+
+    def insert_first(self, data):
+        pass
+
+    def insert_last(self, data):
+        pass
+
+    def remove_first(self):
+        pass
+
+    def remove_last(self):
+        pass
+
+    def remove(self, data):
+        pass
+
+    def get_size(self):  # O(1)
+        pass
+
+    def traverse_list(self):  # O(n)
+        pass
+
