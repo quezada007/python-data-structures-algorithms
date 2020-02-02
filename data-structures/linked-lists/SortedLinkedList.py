d79c97dce00e5b14117d87554f4b20504798abb9
Jose Quezada <quezada007@yahoo.com>
2020-02-01 18:29:18 -0800

    Add Sorted Linked List

---
+++ data-structures/linked-lists/SortedLinkedList.py
@@ -0,0 +1,35 @@
+class Node:
+    """
+    The Node for the Sorted Linked List
+    """
+
+    def __init__(self, data):
+        """
+        The Node constructor
+
+        :param data: The node data
+        """
+        self.data = data
+        self.next = None
+
+
+class SortedLinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def insert(self, data):
+        pass
+
+    def search(self, data):
+        pass
+
+    def remove(self, data):
+        pass
+
+    def get_size(self):  # O(1)
+        pass
+
+    def traverse_list(self):  # O(n)
+        pass
